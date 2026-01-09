89f08344f61d3fa22987f726e186001631dfc789
ainbsyrh <ain040801@gmail.com>
2026-01-09 13:11:06 +0800

    Updated TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,17 +3,25 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
     /**
      * Trust all proxies (Railway / reverse proxy).
+     *
+     * @var array|string|null
      */
     protected $proxies = '*';
 
     /**
-     * Use forwarded headers (so Laravel detect HTTPS correctly).
+     * Use forwarded headers (so Laravel detects HTTPS correctly).
+     *
+     * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+    Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
 }
